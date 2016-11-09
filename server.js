d8e0b276c320be1322eed4882b55efe877cfd8c2
admin <birnaghdejavan@gmail.com>
2016-11-09 19:39:54 +0330

    update commit

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 
 var app = express();
+const PORT=process.env.PORT||3000;
+app.use(function (req,res,next) {
+    if(res.header['x-forwarded-proto']==='http'){
+        next();
+    }
+    else {
+        res.redirect('http://'+req.hostname+req.url);
+    }
+});
 app.use(express.static('public'));
 app.listen(3000, function () {
     console.log('express server is runing on port 3000');
